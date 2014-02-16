0acea945b05bd6c0ef4780cf0b7a46e23723de31
Visnu Pitiyanuvath <visnupx@gmail.com>
2014-02-15 21:31:22 -0800

    FAIR COLOR COUNTS

--- Switches/ViewController.m
+++ Switches/ViewController.m
@@ -13,6 +13,23 @@ typedef NS_ENUM(NSInteger, MessageType) {
   MessageTypeValueChanged
 };
 
+static NSArray *COLORS = nil;
+
+
+@implementation NSMutableArray (Shuffling)
+
+- (void)shuffle
+{
+  NSUInteger count = [self count];
+  for (NSUInteger i = 0; i < count; i++) {
+    // Select a random element between i and end of array to swap with.
+    NSUInteger r = arc4random_uniform((u_int32_t)(count - i)) + i;
+    [self exchangeObjectAtIndex:i withObjectAtIndex:r];
+  }
+}
+
+@end
+
 @interface ViewController ()
 
 @property (strong, nonatomic) MCSession *session;
@@ -27,21 +44,27 @@ typedef NS_ENUM(NSInteger, MessageType) {
 {
   [super viewDidLoad];
 
-  NSString *deviceName = [[UIDevice currentDevice] name];
-  MCPeerID *peerID = [[MCPeerID alloc] initWithDisplayName:deviceName];
-  self.session = [[MCSession alloc] initWithPeer:peerID securityIdentity:nil encryptionPreference:MCEncryptionNone];
+  COLORS = @[ [UIColor colorWithHue:0.3611111111 saturation:0.61 brightness:0.84 alpha:1],
+              [UIColor colorWithHue:0 saturation:0.61 brightness:0.84 alpha:1] ];
+
+  MCPeerID *peerID = [[MCPeerID alloc] initWithDisplayName:[[UIDevice currentDevice] name]];
+  self.session = [[MCSession alloc] initWithPeer:peerID
+                                securityIdentity:nil
+                            encryptionPreference:MCEncryptionNone];
   self.session.delegate = self;
 
   self.advertiser = [[MCAdvertiserAssistant alloc] initWithServiceType:@"visnup-switches"
-                                                         discoveryInfo:nil session:self.session];
+                                                         discoveryInfo:nil
+                                                               session:self.session];
   [self.advertiser start];
 
   NSMutableArray *switches = [NSMutableArray new];
   for (UIView *view in self.view.subviews) {
     if ([view isKindOfClass:[UISwitch class]]) {
-      [(UISwitch *)view addTarget:self action:@selector(valueChanged:) forControlEvents:UIControlEventValueChanged];
-      [(UISwitch *)view setOn:NO animated:NO];
-      view.tag = switches.count;
+      UISwitch *sw = (UISwitch *)view;
+      [sw addTarget:self action:@selector(valueChanged:) forControlEvents:UIControlEventValueChanged];
+      [sw setOn:NO];
+      [sw setTag:switches.count];
       switches[switches.count] = view;
     }
   }
@@ -59,21 +82,40 @@ typedef NS_ENUM(NSInteger, MessageType) {
 
 - (void)valueChanged:(UISwitch *)sender
 {
-  NSDictionary *message = @{ @"type": [NSNumber numberWithInteger:MessageTypeValueChanged],
-                             @"tag": [NSNumber numberWithInteger:sender.tag],
-                             @"on": [NSNumber numberWithBool:sender.on] };
-  NSError *error;
-  [self.session sendData:[NSKeyedArchiver archivedDataWithRootObject:message]
-                 toPeers:self.session.connectedPeers
-                withMode:MCSessionSendDataReliable
-                   error:&error];
-  NSAssert(!error, @"error=%@", error);
+  if (self.session.connectedPeers.count) {
+    NSDictionary *message = @{ @"type": [NSNumber numberWithInteger:MessageTypeValueChanged],
+                               @"tag": [NSNumber numberWithInteger:sender.tag],
+                               @"on": [NSNumber numberWithBool:sender.on] };
+    NSError *error;
+    [self.session sendData:[NSKeyedArchiver archivedDataWithRootObject:message]
+                   toPeers:self.session.connectedPeers
+                  withMode:MCSessionSendDataReliable
+                     error:&error];
+    NSAssert(!error, @"error=%@", error);
+  }
+
+  [self updateScore];
 }
 
 - (BOOL)isLeader
 {
   MCPeerID *leader = [[self.session.connectedPeers sortedArrayUsingSelector:@selector(displayName)] firstObject];
   return [[leader displayName] compare:[self.session.myPeerID displayName]] == NSOrderedAscending;
+}
+
+- (void)updateScore
+{
+  NSUInteger one = 0, two = 0;
+  for (UISwitch *view in self.switches) {
+    if (view.on) {
+      if ([view.onTintColor isEqual:COLORS[0]])
+        one++;
+      else
+        two++;
+    }
+  }
+
+  self.navigationItem.title = [NSString stringWithFormat:@"%lu – %lu", one, two];
 }
 
 #pragma mark - MCBrowserViewControllerDelegate
@@ -88,13 +130,14 @@ typedef NS_ENUM(NSInteger, MessageType) {
   [browserViewController dismissViewControllerAnimated:YES completion:nil];
   if ([self isLeader]) {
     NSMutableArray *colors = [[NSMutableArray alloc] initWithCapacity:self.switches.count];
-    NSMutableArray *teams = [[NSMutableArray alloc] initWithCapacity:2];
+    for (NSUInteger i = 0; i < self.switches.count; i++)
+      colors[i] = [NSNumber numberWithUnsignedInteger:i % COLORS.count];
+    [colors shuffle];
 
-		for (UISwitch *view in self.switches) {
-      NSUInteger color = arc4random_uniform(2);
-			colors[colors.count] = [NSNumber numberWithUnsignedInteger:color];
-      ((UISwitch *)view).onTintColor = color ? [UIColor greenColor] : [UIColor redColor];
-    }
+		for (NSUInteger i = 0; i < self.switches.count; i++)
+      [self.switches[i] setOnTintColor:COLORS[[colors[i] unsignedIntegerValue]]];
+
+    NSMutableArray *teams = [[NSMutableArray alloc] initWithCapacity:2];
 
     NSDictionary *message = @{ @"type": [NSNumber numberWithInteger:MessageTypeSetup],
                                @"colors": colors,
@@ -124,7 +167,8 @@ typedef NS_ENUM(NSInteger, MessageType) {
       NSArray *colors = message[@"colors"];
       dispatch_async(dispatch_get_main_queue(), ^{
         for (NSUInteger i = 0; i < self.switches.count; i++) {
-          ((UISwitch *)self.switches[i]).onTintColor = [colors[i] unsignedIntegerValue] ? [UIColor greenColor] : [UIColor redColor];
+          [self.switches[i] setOnTintColor:COLORS[[colors[i] unsignedIntegerValue]]];
+          [self.switches[i] setOn:NO];
         }
       });
       break;
@@ -132,6 +176,7 @@ typedef NS_ENUM(NSInteger, MessageType) {
     case MessageTypeValueChanged:
       dispatch_async(dispatch_get_main_queue(), ^{
         [self.switches[[message[@"tag"] longValue]] setOn:[message[@"on"] boolValue] animated:YES];
+        [self updateScore];
       });
       break;
   }
